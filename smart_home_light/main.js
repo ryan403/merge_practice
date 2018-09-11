4b77e6c4e7a02b7f83ea67b4e9b1650e71899d54
epicwoo <epicwoo@gmail.com>
2018-09-11 16:45:23 +0800

    180911

---
+++ smart_home_light/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    getLightSwitchStatus()
+});
+
+function getLightSwitchStatus(){
+    var url = "https://sheetdb.io/api/v1/5b9609c2b53e5/";
+    var data = $.getJSON(url,{
+        light_name:"main"
+    })
+    .done(
+        function(msg){
+            console.log(msg);
+            $("h1").text("電燈狀態："+msg[0].light_switch);
+            if(msg[0].light_switch=="開"){
+                $("img").attr("src","smart_home_light/images/pic_bulbon.gif");
+            }else{
+                $("img").attr("src","smart_home_light/images/pic_bulboff.gif");
+            }
+        }
+    )
+    .fail(
+        function(msg){
+            console.log("fail!");
+        }
+    )
+    .always(
+        function(msg){
+            console.log("complete!");
+        }
+    );
+}
